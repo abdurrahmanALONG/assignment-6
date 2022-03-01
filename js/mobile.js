dfada9ba82fcd23e07f90d536ed025b6852bf1af
abdurrahmanALONG <abdullahhanxin@gmail.com>
2022-03-01 20:57:07 +0600

    third commit

--- js/mobile.js
+++ js/mobile.js
@@ -1,7 +1,11 @@
 const searchPhone = () => {
   const searchFild = document.getElementById('search-field');
   const searchText = searchFild.value;
-//   console.log(searchText);
+  searchFild.value ='';
+  console.log(searchText);
+// const url = ` https://openapi.programming-hero.com/api/phones?search=${searchText}`
+// .then(res => res.json())
+// .then(data => console.log(data));
   const url =`https://openapi.programming-hero.com/api/phones?search=${searchText}`
   fetch(url)
   .then(res => res.json())
@@ -13,19 +17,57 @@ const displayPhones = phones => {
     // console.log(phones);
     const searchResult = document.getElementById('search-result');
     phones.forEach(phone => {
-        console.log(phone);
+        // console.log(phone.slug);
         const div = document.createElement('div');
         div.classList.add('col');
+        // const id = `${slug}`;
         div.innerHTML = `
-            <div class="card h-100">
-              <img src="" class="card-img-top" alt="">
+            <div  class="card h-50">
+              <img src="${phone.image}" class="card-img-top" alt="">
                <div class="card-body">
-                 <h5 class="card-title">Card title</h5>
-                 <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                 <h4 class="card-title">${phone.phone_name}</h4>
+                 <h5 class="card-title">${phone.brand}</h5>
                </div>
+               <button onclick="searchDitails('${phone.slug}')" class=" w-25 mx-auto">deatils</button>
             </div>
         `;
         searchResult.appendChild(div);
 
     })
+}
+
+
+
+
+
+
+
+
+
+
+
+const searchDitails = details => {
+    console.log(details);
+    const url =`https://openapi.programming-hero.com/api/phone/${details}`
+    fetch(url)
+    .then(res => res.json())
+    .then(data =>  displayDetails(data.data));
+}
+
+const displayDetails = phoneDetails => {
+    console.log(phoneDetails);
+    const detailsResult = document.getElementById('details-button');
+        const div = document.createElement('div');
+        div.classList.add('card');
+        div.innerHTML = `
+        <div class="card h-50 mx-auto">
+            <img src="${phoneDetails.image}" class="card-img-top" alt="">
+            <div class="card-body">
+            <h5 class="card-title">${phoneDetails.brand}</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+       </div>
+     </div>
+        `;
+        detailsResult.appendChild(div);
 }
--- js/test.js
+++ js/test.js
@@ -1,14 +1,18 @@
 const autoQuato = () => {
-    fetch('https://api.kanye.rest')
-    .then(res => res.json())
-    .then(data => getQuote(data));
+    // fetch('https://openapi.programming-hero.com/api/phone/apple_iphone_13_pro_max-11089')
+    // .then(res => res.json())
+    // .then(data => getQuote(data));
 }
 
-const getQuote = quote => {
-    // console.log(quote.quote);
-    const getDiv = document.getElementById('list');
-    getDiv.innerText = quote.quote;
-    // console.log(getDiv.innerText);
-    const p = document.createElement('p');
-    getDiv.appendChild(p);
-}
+// const getQuote = quote => {
+//     // console.log(quote.quote);
+//     const getDiv = document.getElementById('list');
+//     getDiv.innerText = quote.quote;
+//     // console.log(getDiv.innerText);
+//     const p = document.createElement('p');
+//     getDiv.appendChild(p);
+// }
+
+fetch('https://openapi.programming-hero.com/api/phone/apple_iphone_13_pro_max-11089')
+    .then(res => res.json())
+    .then(data => console.log(data));
